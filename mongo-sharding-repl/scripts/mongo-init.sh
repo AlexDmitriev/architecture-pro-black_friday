eaecc44e7bb216379991ac78030932a3f99a4efe
Alex Dmitriev <alex@alexdmitriev.com>
2026-03-02 16:50:58 +0300

    Task 3

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -1,60 +1,66 @@
 #!/bin/bash
+set -e
 
 ###
 # Инициализируем бд
 ###
 echo "Init configSrv"
-docker exec -i configSrv mongosh --port 27017 <<EOF
+docker exec -i configSrv mongosh --port 27010 <<EOF
 rs.initiate(
   {
     _id : "config_server",
        configsvr: true,
     members: [
-      { _id : 0, host : "configSrv:27017" }
+      { _id : 0, host : "configSrv:27010" }
     ]
   }
 );
 EOF
+sleep 5
 echo "Init shard1"
-docker exec -i shard1 mongosh --port 27018 <<EOF
+docker exec -i shard11 mongosh --port 27011 <<EOF
 rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id : 0, host : "shard1:27018" },
-       // { _id : 1, host : "shard2:27019" }
+        { _id : 0, host : "shard11:27011" },
+        { _id : 1, host : "shard12:27012" },
+        { _id : 2, host : "shard13:27013" }
       ]
     }
 );
 EOF
+sleep 5
 echo "Init shard2"
-docker exec -i shard2 mongosh --port 27019 <<EOF
+docker exec -i shard21 mongosh --port 27014 <<EOF
 rs.initiate(
     {
       _id : "shard2",
       members: [
-       // { _id : 0, host : "shard1:27018" },
-        { _id : 1, host : "shard2:27019" }
+        { _id : 0, host : "shard21:27014" },
+        { _id : 1, host : "shard22:27015" },
+        { _id : 2, host : "shard23:27016" }
       ]
     }
   );
 EOF
+sleep 5
 echo "Init router"
 docker compose exec -T mongodb1 mongosh --port 27020 <<EOF
-sh.addShard( "shard1/shard1:27018");
-sh.addShard( "shard2/shard2:27019");
+sh.addShard( "shard1/shard11:27011,shard12:27012,shard13:27013");
+sh.addShard( "shard2/shard21:27014,shard22:27015,shard23:27016");
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
 use somedb
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
 echo "Docs in shard1"
-docker exec -i shard1 mongosh --port 27018 <<EOF
+docker exec -i shard11 mongosh --port 27011 <<EOF
 use somedb;
 db.helloDoc.countDocuments()
 EOF
 echo "Docs in shard2"
-docker exec -i shard2 mongosh --port 27019 <<EOF
+docker exec -i shard21 mongosh --port 27014 <<EOF
 use somedb;
 db.helloDoc.countDocuments()
 EOF
